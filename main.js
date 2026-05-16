3e50e8ccd1a7aa318528bd4607a50ddad9662d8e
819605834 <124411012+819605834@users.noreply.github.com>
2026-05-16 00:59:47 +0000

    Create mad lib

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-var adjective1 = prompt('Provide an adjective.');
-var adjective2 = prompt('Provide another adjective.');
-var adverb = prompt('Provide an adverb.');
+var adjective1 = prompt('Provide an adjective.', 'bad');
+var adjective2 = prompt('Provide another adjective.', 'silly');
+var adverb = prompt('Provide an adverb.', 'jump');
 
 // Write your code below
+var madLib = `
+Today I had a very ${adjective1} and ${adjective2} day.
+I decided to ${adverb} all afternoon to feel better.
+`;
+console.log(madLib);
